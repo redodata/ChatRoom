23b68e3ece79e3717aad7b2bac08295e4767e13a
redodata <43987661+redodata@users.noreply.github.com>
2019-06-04 15:10:07 +0800

    Update MultiServer.java

--- chat-room-server/src/main/java/com/xunpu/MultiThreadServer/MultiServer.java
+++ chat-room-server/src/main/java/com/xunpu/MultiThreadServer/MultiServer.java
@@ -20,6 +20,11 @@ public class MultiServer {
             ServerSocket serverSocket=new ServerSocket(port);
             System.out.println("服务器启动 "+serverSocket.getLocalSocketAddress());
             System.out.println("等待客户端连接。。。。。");
+            System.out.println("注册请输入userName:用户名");
+            System.out.println("私聊请输入privateChat:要私聊的用户名:私聊内容");
+            System.out.println("群聊请输入groupChat:群聊内容");
+            System.out.println("退出请输入bye");
+
             //为什么要使用newFixedThreadPool方法创建线程池？
             //设置最多同时有10个用户在线
 
