042323831e4e06db186bf0ffac907d85c25f24a6
redodata <43987661+redodata@users.noreply.github.com>
2019-06-07 10:58:19 +0800

    Update SingleThreadServer.java

--- chat-room-server/src/main/java/com/xunpu/SingleThreadChat/SingleThreadServer.java
+++ chat-room-server/src/main/java/com/xunpu/SingleThreadChat/SingleThreadServer.java
@@ -1,4 +1,4 @@
-package com.xunpu.SingleThreadChat;
+package com.xunpu.client.SingleThreadChat;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,7 +12,7 @@ public class SingleThreadServer {
     public static void main(String[] args) throws IOException {
         //创建ServerSocket对象
         ServerSocket serverSocket=new ServerSocket(6666);
-        System.out.println("客户端启动，等待连接。。。。");
+        System.out.println("服务器端启动，等待连接。。。。");
 
         //接收Socket对象
         Socket client=serverSocket.accept();
@@ -21,14 +21,19 @@ public class SingleThreadServer {
         //接收信息
         InputStream clientInput=client.getInputStream();
         Scanner scanner=new Scanner(clientInput);
-        String message=scanner.nextLine();
-        System.out.println("客户端对服务端说："+message);
-
         //发送信息
         OutputStream clientOutput=client.getOutputStream();
-        OutputStreamWriter writer=new OutputStreamWriter(clientOutput);
-        writer.write("你好，客户端\n");
-        writer.flush();
+        while(scanner.hasNext()){
+            String message = scanner.nextLine();
+            System.out.println("客户端对服务端说：" + message);
+           //发送消息
+            OutputStreamWriter writer=new OutputStreamWriter(clientOutput);
+            writer.write("你好，客户端。正在处理您的信息\n");
+            writer.flush();
+        }
+        scanner.close();
+        clientInput.close();
+        clientOutput.close();
 
     }
 
