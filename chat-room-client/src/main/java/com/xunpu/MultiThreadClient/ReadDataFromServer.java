6de90c64659a11dfd1c44dfa39b40341c09aaedb
redodata <43987661+redodata@users.noreply.github.com>
2019-06-07 16:18:00 +0800

    Update ReadDataFromServer.java

--- chat-room-client/src/main/java/com/xunpu/MultiThreadClient/ReadDataFromServer.java
+++ chat-room-client/src/main/java/com/xunpu/MultiThreadClient/ReadDataFromServer.java
@@ -1,4 +1,4 @@
-package com.xunpu.MultiThreadClient;
+package com.xunpu.client.MultiThreadClient;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,18 +13,22 @@ public class ReadDataFromServer extends Thread{
     }
 
     public void run() {
-        try {
-            InputStream clientInput=this.client.getInputStream();
-            Scanner scanner=new Scanner(clientInput);
-            String message;
-           do {
-                message = scanner.nextLine();
-                System.out.println("来自服务器的消息：" + message);
-            }while(!message.equals("bye"));
-            clientInput.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
+            try {
+                InputStream clientInput = this.client.getInputStream();
+                Scanner scanner = new Scanner(clientInput);
+             while(scanner.hasNext()){
+                 String message=scanner.next();
+                 System.out.println("来自服务器的消息："+message);
+                 if(message.equals("bye")){
+                     break;
+                 }
+             }
+             clientInput.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
 }
+
+
